5deed9c4ccf92950d4d9452645c767d6183675ad
Jereza <cjjereza@186590cfe04f.ant.amazon.com>
2017-07-21 15:40:36 -0700

    Added data.js, fixing bugs in index, need to implement handlers

---
+++ conversions.js
@@ -0,0 +1,34 @@
+/**
+ * File containing conversions from various units of measurement to grams.
+ * @author Unity
+ */
+
+/**
+ * Convert a quantity and unit to grams
+ * RETURN TYPE: float
+ */
+function getGrams(quantity, unit) {
+    if (unit === 'ounces' || unit === 'ounce') {
+        return quantity * 28.3495;
+    } else if (unit === 'grams' || unit === 'gram') {
+        return quantity;
+    } else if (unit === 'fluid ounces' || unit === 'fluid ounce') {
+        return quantity * 29.5735296875;
+    } else if (unit === 'pounds' || unit === 'pound') {
+        return quantity * 453.592;
+    } else if (unit === 'cups' || unit === 'cup') {
+        return quantity * 236.5882375;
+    } else if (unit === 'tablespoons' || unit === 'tablespoon') {
+        return quantity * 14.7867648437
+    } else if (unit === 'teaspoons' || unit === 'teaspoon') {
+        return quantity * 4.92892161457;
+    } else if (unit === 'liters' || unit === 'liter') {
+        return quantity * 1000;
+    } else if (unit === 'quarts' || unit === 'quart') {
+        return quantity * 946.35295;
+    } else if (unit === 'gallons' || unit === 'gallon') {
+        return quantity * 3785.4118;
+    }
+}
+
+exports.getGrams = getGrams;
--- index.js
+++ index.js
@@ -2,22 +2,40 @@ var Alexa = require('alexa-sdk');
 
 const WHITESPACE = ' ';
 
+const STATES = {
+    'HELP': '_HELPSTATE',
+    'MAIN': '_MAINSTATE',
+    'WHATIF': '_WHATIFSTATE'
+}
+
 const MESSAGES = {
     'WELCOME': 'Welcome to Food Buddy! Add an ingredient or food item to your meal.',
+    'STOP': 'Goodbye!',
+    'HELP': '',
+    'UNHANDLED': 'I\'m sorry, I didn\'t get that. Please specify a quantity, ' +
+                 'a unit of measurement, and an ingredient. Or, say REPORT.',
+    'START_UNHANDLED': 'Say, \'Open Food Buddy\', to track the nutritional value of your meal.'
 };
+
+const DATA = require('./data.js');
 
 function add(quantity, unit, ingredient) {
     if (this.attributes.meal === undefined) {
         this.attributes.meal = {};
     }
-    this.attributes.meal[ingredient] = [quantity, unit];
+    if (this.attributes.ingredients === undefined) {
+        this.attributes.ingredients = {};
+    }
+    const macros = DATA.getMacros(quantity, unit, ingredient);
+    this.attributes.meal[ingredient] = macros;
+    this.attributes.ingredients[ingredient] = [quantity][unit];
     this.attributes.lastItemAdded = ingredient;
-    this.emit(':ask', 'Adding ' + stringify.call(this, quantity, unit, ingredient));
+    this.emit(':ask', 'Adding ' + stringify.call(this, quantity, unit, ingredient, macros.Calories));
 }
 
 /* Return a string with the quantity, unit, and ingredient of the added item. */
-function stringify(quantity, unit, ingredient) {
-    return quantity + WHITESPACE + unit + ' of ' + ingredient;
+function stringify(quantity, unit, ingredient, calories) {
+    return quantity + WHITESPACE + unit + ' of ' + ingredient + ' with ' + calories + ' calories.';
 }
 
 /* List every ingredient added so far. */
@@ -26,7 +44,37 @@ function report() {
     if (meal.length <= 0) {
         return 'No food items have been added.';
     }
-    let outputSpeech = 'You have added ';
+    let totalCalories = 0;
+    let totalCarbs = 0;
+    let totalProtein = 0;
+    let totalFat = 0;
+    let macros;
+    for (let ingredient in meal) {
+        if (meal.hasOwnProperty(ingredient) && meal[ingredient] !== undefined) {
+            macros = meal[ingredient];
+            totalCalories += macros.Calories;
+            totalCarbs += macros.Carbs;
+            totalProtein += macros.Protein;
+            totalFat += macros.Fat;
+        }
+    }
+    let outputSpeech = 'You have a total of ' + totalCalories + ' Calories, ' + totalCarbs + ' grams of carbohydrates, ' +
+                        totalProtein + ' grams of protein, and ' + totalFat + ' grams of fat.';
+    this.emit(':ask', outputSpeech);
+}
+
+function remove() {
+    let meal = this.attributes.meal;
+    let itemToDelete = this.attributes.lastItemAdded;
+    delete meal[itemToDelete];
+    this.attributes.meal = meal;
+    let outputSpeech = 'Removing ' + itemToDelete;
+    this.emit(':ask', outputSpeech);
+}
+
+function ingredients() {
+    let meal = this.attributes.ingredients;
+    let outputSpeech = 'You have added: ';
     let ingredients = [];
     for (let ingredient in meal) {
         if (meal.hasOwnProperty(ingredient) && meal[ingredient] !== undefined) {
@@ -60,16 +108,40 @@ function report() {
     this.emit(':ask', outputSpeech);
 }
 
-function remove() {
-    let meal = this.attributes.meal;
-    let itemToDelete = this.attributes.lastItemAdded;
-    delete meal[itemToDelete];
-    this.attributes.meal = meal;
-    let outputSpeech = 'Removing ' + itemToDelete;
-    this.emit(':ask', outputSpeech);
-}
-
 var handlers = {
+    'LaunchRequest': function () {
+        this.handler.state = STATES.MAIN;
+        this.emitWithState('LaunchRequest');
+    },
+    'AddIntent': function () {
+        this.handler.state = STATES.MAIN;
+        this.emitWithState('AddIntent');
+    },
+    'WhatIfIntent': function() {
+        this.handler.state = STATES.MAIN;
+        this.emitWithState('WhatIfIntent');
+    },
+    'ReportIntent': function() {
+        this.handler.state = STATES.MAIN;
+        this.emitWithState('ReportIntent');
+    },
+    'RemoveIntent': function() {
+        this.handler.state = STATES.MAIN;
+        this.emitWithState('RemoveIntent');
+    },
+    'IngredientsIntent': function() {
+        this.handler.state = STATES.MAIN;
+        this.emitWithState('IngredientsIntent');
+    },
+    'Unhandled': function() {
+        this.emit(':tell', MESSAGES.START_UNHANDLED);
+    },
+    'AMAZON.StopIntent': function() {
+        this.emit(':tell', MESSAGES.STOP);
+    },
+};
+
+const mainStateHandler = Alexa.CreateStateHandler(STATES.MAIN, {
     'LaunchRequest': function () {
         this.attributes.meal = {};
         this.emit(':ask', MESSAGES.WELCOME);
@@ -89,14 +161,30 @@ var handlers = {
     'RemoveIntent': function() {
         remove.call(this);
     },
+    'IngredientsIntent': function() {
+        ingredients.call(this)
+    },
     'Unhandled': function() {
-        this.emit(':ask', 'I\'m sorry, I didn\'t get that. Please specify a quantity, ' +
-                          'a unit of measurement, and a food item. Or, say REPORT');
+        this.emit(':ask', MESSAGES.UNHANDLED);
     },
     'AMAZON.StopIntent': function() {
-        this.emit(':tell', '')
+        this.emit(':tell', MESSAGES.STOP);
+    },
+    'AMAZON.HelpIntent': function() {
+        this.handler.state = STATES.HELP;
+        this.emit(':tell', MESSAGES.HELP);
     }
-};
+});
+
+const whatIfStateHandler = Alexa.CreateStateHandler(STATES.WHATIF, {
+
+});
+
+const helpStateHandler = Alexa.CreateStateHandler(STATES.HELP, {
+
+});
+
+
 
 
 
