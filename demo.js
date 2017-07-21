94d1011eb07a90c5956fbd297e395538ca0e472b
Lo <loemily@186590ce1bc1.ant.amazon.com>
2017-07-21 13:26:10 -0700

    Customize search query

--- demo.js
+++ demo.js
@@ -1,20 +1,42 @@
-var NutrientDataLaboratory = require("nutrient-data-laboratory");
+const NutrientDataLaboratory = require("nutrient-data-laboratory");
 
 let ndl = new NutrientDataLaboratory('Zo7ru5UezyJCdMFmNSwqBnZiQUKysRvoRx9skNOc');
-
-ndl.foodReports({
-  ndbno: '01009',
-  type: 'b'
-}, (err, res) => {
-  if (err) console.error(err)
-  else console.log(res.report.food.nutrients[1].value + " " + res.report.food.nutrients[1].unit);
-})
+const searchQuery = 'carrots';
 
 ndl.search({
-  q: 'butter',
+  q: searchQuery,
   max: 25,
   offset: 0
-}, (err, res) => {
-  if (err) console.error(err)
-  else console.log(res.list.item[0].ndbno);
-})
+  }, (err, res) => {
+  if (err) {
+    console.error(err);
+  }
+  else {
+    //console.log(res.list.item);
+    let ndbnoQuery = res.list.item[0].ndbno;
+    console.log(searchQuery + " ndbno is: " + ndbnoQuery);
+    ndl.foodReports({
+      ndbno: ndbnoQuery,
+      type: 'b'
+    }, (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+      else {
+        let energyIndex = findEnergy(result.report.food.nutrients);
+        console.log("100 grams of " + searchQuery + " is " +
+        result.report.food.nutrients[energyIndex].value + " " +
+        result.report.food.nutrients[energyIndex].unit);
+      }
+    });
+  }
+});
+
+function findEnergy(input){
+  for (let i = 0; i < input.length; i++){
+    if (input[i].name === 'Energy'){
+      return i;
+    }
+  }
+  return -1;
+}
